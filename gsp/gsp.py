d964b2fe5e69af324d6ded384c1ef30227e19f6d
mrtplk <martapalka13@gmail.com>
2021-05-17 21:20:28 +0200

    Add saving gsp results to csv

--- gsp/gsp.py
+++ gsp/gsp.py
@@ -3,6 +3,7 @@ from input_parser.input_parser import parse_file
 from algorithm.algorithm import search
 from model.sequence import Sequence
 from algorithm.support_counting import count_support
+from experiments.experiment import save_experiment_to_csv
 
 file_name = sys.argv[1]
 sequences = parse_file(file_name)
@@ -11,8 +12,15 @@ for sequence in sequences:
 
 min_sup = float(sys.argv[2])
 
+experiment = 'astra' #ustawić w zależności od eksperymentu
+
 result = search(min_sup, sequences)
 print("Results")
+
+data_ = []
 for sequence in result:
     seq_sup = count_support(sequence, sequences)
     print("Seq: {}, sup: {}".format(seq_sup[0], seq_sup[1]))
+    data_.append([seq_sup[0], seq_sup[1]])
+
+save_experiment_to_csv(data_, 'astra', 'experiments/')
