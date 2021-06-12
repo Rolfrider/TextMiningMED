e861f361e3d17f9a7bb37f399d62ae0c5ca81077
mrtplk <martapalka13@gmail.com>
2021-06-13 00:45:15 +0200

    Add visualisation of frequent pattrens

--- gsp/gsp.py
+++ gsp/gsp.py
@@ -5,7 +5,7 @@ from algorithm.algorithm_improve import search as improve_search
 from model.sequence import Sequence
 from algorithm.support_counting import count_support
 from experiments.experiment import save_experiment_to_csv
-#from experiments.visualise import Visualise
+from experiments.visualise import Visualise
 
 file_name = sys.argv[1]
 sequences = parse_file(file_name)
@@ -25,8 +25,11 @@ for sequence in result:
     seq_sup = count_support(sequence, sequences)
     print("Seq: {}, sup: {}".format(seq_sup[0], seq_sup[1]))
     data_.append([seq_sup[0], seq_sup[1]])
-    #data_viz.append(sequence.flatten_items)
+    data_viz.append(seq_sup[0])
 
+#visualiser = Visualise( [['A', 'B'], ['A,C', 'D']])
+visualiser = Visualise(data_viz)
+visualiser.draw()
 
 result = improve_search(min_sup, sequences)
 print("Results improved")
